ee0262728a89f2cc3095927c59afe4f9569217fb
shreyahunur <shreyahunur@gmail.com>
2022-10-19 22:12:47 -0700

    Add procbased controls code

--- Assignment-1/cmpe283-1.c
+++ Assignment-1/cmpe283-1.c
@@ -79,6 +79,71 @@ struct capability_info entrybasedcontrols[13] =
     { 22, "Load PKRS"}
 };
 
+struct capability_info procbasedcontrols[22] =
+{
+    { 2, "Interrupt-window exiting" },
+    { 3, "Use TSC offsetting" },
+    { 7, "HLT exiting" },
+    { 9, "INVLPG exiting" },
+    { 10, "MWAIT exiting" },
+    { 11, "RDPMC exiting" },
+    { 12, "RDTSC exiting" },
+    { 15, "CR3-load exiting" },
+    { 16, "CR3-store exiting" },
+    { 17, "Activate tertiary controls" },
+	{ 19, "CR8-load exiting"}
+    { 20, "CR8-store exiting" },
+    { 21, "Use TPR shadow" },
+    { 22, "NMI-window exiting"},
+	{ 23, "MOV-DR exiting" },
+    { 24, "Unconditional I/O exiting" },
+    { 25, "Use I/O bitmaps" },
+    { 27, "Monitor trap flag" },
+    { 28, "Use MSR bitmaps" },
+	{ 29, "MONITOR exiting"}
+    { 30, "PAUSE exiting" },
+    { 31, "Activate secondary controls" }
+};
+
+struct capability_info procbasedcontrols2[28] =
+{
+	{ 0, "Virtualize APIC accesses" },
+	{ 1, "Enable EPT" },
+    { 2, "Descriptor-table exiting" },
+    { 3, "Enable RDTSCP" },
+	{ 4, "Virtualize x2APIC mode" },
+	{ 5, "Enable VPID" },
+	{ 6, "WBINVD exiting" },
+    { 7, "Unrestricted guest" },
+	{ 8, "APIC-register virtualization" },
+    { 9, "Virtual-interrupt delivery" },
+    { 10, "PAUSE-loop exiting" },
+    { 11, "RDRAND exiting" },
+    { 12, "Enable INVPCID" },
+	{ 13, "Enable VM functions" },
+	{ 14, "VMCS shadowing" },
+    { 15, "Enable ENCLS exiting" },
+    { 16, "RDSEED exiting" },
+    { 17, "Enable PML" },
+	{ 18, "EPT-violation #VE" },
+	{ 19, "Conceal VMX from PT"}
+    { 20, "Enable XSAVES/XRSTORS" },
+    { 22, "Mode-based execute control for EPT"},
+	{ 23, "Sub-page write permissions for EPT" },
+    { 24, "Intel PT uses guest physical addresses" },
+    { 25, "Use TSC scaling" },
+	{ 26, "Enable user wait and pause" },
+    { 27, "Enable PCONFIG" },
+    { 28, "Enable ENCLV exiting" }
+};
+
+struct capability_info procbasedcontrols3[4] =
+{
+	{ 0, "LOADIWKEY exiting" },
+	{ 1, "Enable HLAT" },
+	{ 2, "EPT paging-write control" },
+	{ 3, "Guest-paging verification" }
+};
 /*
  * report_capability
  *
@@ -128,6 +193,9 @@ detect_vmx_features(void)
 	report_capability(pinbased, 5, lo, hi);
 	report_capability(exitbasedcontrols, 17, lo, hi);
 	report_capability(entrybasedcontrols, 13, lo, hi);
+	report_capability(procbasedcontrols, 22, lo, hi);
+	report_capability(procbasedcontrols2, 28, lo, hi);
+	report_capability(procbasedcontrols3, 4, lo, hi);
 }
 
 /*
