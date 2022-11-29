dd02410e2a6c65306796f35b065c0b98b6448396
shreyahunur <shreyahunur@gmail.com>
2022-11-28 16:04:38 -0800

    Add Assignment-2

---
+++ Assignment-2/A1/.cmpe283-1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/cmpe283/A1/cmpe283-1.ko := ld -r  -m elf_x86_64 -z noexecstack   --build-id=sha1  -T scripts/module.lds -o /home/cmpe283/A1/cmpe283-1.ko /home/cmpe283/A1/cmpe283-1.o /home/cmpe283/A1/cmpe283-1.mod.o;  true
---
+++ Assignment-2/A1/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/cmpe283/A1/modules.order := {   echo /home/cmpe283/A1/cmpe283-1.ko; :; } | awk '!x[$$0]++' - > /home/cmpe283/A1/modules.order
